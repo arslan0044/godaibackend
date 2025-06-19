8b848267a1dd44731087f42fece84951cd3f06c7
arslan0044 <72161665+arslan0044@users.noreply.github.com>
2025-06-19 15:10:04 +0500

    refactor: Improve code formatting and enhance readability in blogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,29 +1,32 @@
-const Reminder = require('../models/Reminder');
-
+const Reminder = require("../models/Reminder");
 
 exports.createPost = async (req, res) => {
   try {
-    const { title, reminderAt, remind, priority,quantity,description,type } = req.body;
+    const { title, reminderAt, remind, priority, quantity, description, type } =
+      req.body;
     const userId = req.user._id;
 
-  
-    const blog = new Reminder({ 
+    const blog = new Reminder({
       user: userId,
-      title, 
-      reminderAt, 
-      remind, 
+      title,
+      reminderAt,
+      remind,
       priority,
-      quantity:quantity||"",
-      description:description||"" ,
-      type
+      quantity: quantity || "",
+      description: description || "",
+      type,
     });
-    
+
     await blog.save();
 
-    res.status(201).json({ success: true, message: 'Reminder created successfully', reminder:blog });
+    res.status(201).json({
+      success: true,
+      message: "Reminder created successfully",
+      reminder: blog,
+    });
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
@@ -31,69 +34,102 @@ exports.editpost = async (req, res) => {
   try {
     const courseId = req.params.id;
 
-    const { title, reminderAt, repeat, remind, priority,description  } = req.body;
+    const {
+      title,
+      reminderAt,
+      repeat,
+      remind,
+      priority,
+      quantity,
+      description,
+    } = req.body;
 
-      // Create an object to store the fields to be updated
-      const updateFields = Object.fromEntries(
-        Object.entries({
-          title, reminderAt, repeat, remind, priority,description
-        }).filter(([key, value]) => value !== undefined)
-      );
-    
-      // Check if there are any fields to update
-      if (Object.keys(updateFields).length === 0) {
-        return res
-          .status(400)
-          .send({
-            success: false,
-            message: "No valid fields provided for update.",
-          });
-      }
+    // Create an object to store the fields to be updated
+    const updateFields = Object.fromEntries(
+      Object.entries({
+        title,
+        reminderAt,
+        repeat,
+        remind,
+        priority,
+        description,
+        quantity,
+      }).filter(([key, value]) => value !== undefined)
+    );
 
-    const medicine = await Reminder.findByIdAndUpdate(courseId,updateFields,{ new: true });
+    // Check if there are any fields to update
+    if (Object.keys(updateFields).length === 0) {
+      return res.status(400).send({
+        success: false,
+        message: "No valid fields provided for update.",
+      });
+    }
 
-    if (!medicine) return res.status(200).json({ success: false, message: "Invalid Reminder ID"});
+    const medicine = await Reminder.findByIdAndUpdate(courseId, updateFields, {
+      new: true,
+    });
 
-    res.status(201).json({ success: true, message: "Reminder updated successfully", reminder:medicine });
+    if (!medicine)
+      return res
+        .status(200)
+        .json({ success: false, message: "Invalid Reminder ID" });
+
+    res.status(201).json({
+      success: true,
+      message: "Reminder updated successfully",
+      reminder: medicine,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
-exports.getAllPosts = async (req, res) => {  
+exports.getAllPosts = async (req, res) => {
   try {
-    const {repeat, remind, priority,type}=req.query;
-  // Create an object to store the fields to be updated
-  const updateFields = Object.fromEntries(
-    Object.entries({
-      repeat, remind, priority,type
-    }).filter(([key, value]) => value !== undefined)
-  );
+    const { repeat, remind, priority, type } = req.query;
+    // Create an object to store the fields to be updated
+    const updateFields = Object.fromEntries(
+      Object.entries({
+        repeat,
+        remind,
+        priority,
+        type,
+      }).filter(([key, value]) => value !== undefined)
+    );
 
-  console.log(updateFields)
+    console.log(updateFields);
 
-    const blog = await Reminder.find({ user:req.user._id,...updateFields }).sort({ _id: -1 }).lean();
+    const blog = await Reminder.find({ user: req.user._id, ...updateFields })
+      .sort({ _id: -1 })
+      .lean();
 
     if (blog.length > 0) {
       res.status(200).json({ success: true, reminder: blog });
     } else {
-      res.status(200).json({ success: false,reminder:[], message: 'No more Reminders found' });
+      res.status(200).json({
+        success: false,
+        reminder: [],
+        message: "No more Reminders found",
+      });
     }
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Internal server error' });
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
-
 
 exports.deletePost = async (req, res) => {
   const postId = req.params.id;
   try {
     const blog = await Reminder.findByIdAndDelete(postId).lean();
 
-    res.status(200).json({ success: true, reminder: blog,message:"Reminder deleted successfully!" });
+    res.status(200).json({
+      success: true,
+      reminder: blog,
+      message: "Reminder deleted successfully!",
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
